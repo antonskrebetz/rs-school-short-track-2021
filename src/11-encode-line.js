912e44325524c350d084d6d37c98975ff02db8ac
antonskrebetz <antonskrebetz@mail.ru>
2021-04-13 00:09:21 +0300

    add ready task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      arr.splice(i + 1, 1);
+      count += 1;
+      i--;
+    } else if (count > 1) {
+      arr[i] = `${count}${arr[i]}`;
+      count = 1;
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = encodeLine;
